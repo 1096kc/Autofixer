4e2d5ad064731d64ae0e1724632b0d076597fa09
Victor <1412621@mail.nankai.edu.cn>
2019-11-26 13:24:06 -0500

    a

---
+++ AutoFixer/src/main/resources/javaparser/modified/java/com/github/javaparser/ast/nodeTypes/NodeWithCondition.java
@@ -0,0 +1,11 @@
+package com.github.javaparser.ast.nodeTypes;
+
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.Expression;
+
+public interface NodeWithCondition<N extends Node> {
+
+    Expression getCondition();
+
+    N setCondition(Expression condition);
+}
---
+++ AutoFixer/src/main/resources/patterns/Pattern9.java
@@ -0,0 +1,10 @@
+public class Pattern9{
+    int DATEOFBIRTH;
+    public Pattern5(int DATE_OF_BIRTH) {
+        this.DATE_OF_BIRTH = DATE_OF_BIRTH;
+    }
+
+    public int Get_DATE_OF_BITRH(){
+        return this.DATE_OF_BIRTH;
+    }
+}
